bc6fb671de62a5a3218253d6bd27b06886889248
Vankeep <92756532+Vankeep@users.noreply.github.com>
2024-06-03 12:35:38 +0300

    Иницализация значениями 1500 и ошибка в read

--- CircularBuffer.h
+++ CircularBuffer.h
@@ -1,29 +1,37 @@
 #ifndef _CircularBuffer_h
 #define _CircularBuffer_h
+
 template < typename TYPE, int SIZE >
 class CircularBuffer {
 public:
-     void write(T t){
+    CircularBuffer() {
+        for (int i = 0; i < SIZE; i++) {
+            buffer[i] = 1500;
+        }
+    }
+
+    void write(TYPE t) {
         buffer[tail] = t;
         tail = (tail + 1) % SIZE;
         head = tail == 0 ? SIZE - 1 : tail - 1;
-     }
+    }
 
-     T readTail(){
+    TYPE readTail() {
         return buffer[tail];
-     }
+    }
 
-     T readHead(){
+    TYPE readHead() {
         return buffer[head];
-     }
+    }
 
-     T read(int index){
-        if (index > size) return 0;
+    TYPE read(int index) {
+        if (index >= SIZE) return 0;
         return buffer[(tail + index) % SIZE];
-     }
+    }
 
 private:
-    TYPE buffer[SIZE + 1];
+    TYPE buffer[SIZE];
     int head = 0, tail = 0;
 };
+
 #endif
